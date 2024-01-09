d52c17f674dec41178b5a666afd153e44d2d9d8c
CharuSachindu <107404035+CharuSachindu@users.noreply.github.com>
2024-01-09 23:30:48 +0530

    Add files via upload

---
+++ customer-application/src/components/Buttons/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './HeroButton.module.css'
+function Button() {
+    return (
+       
+        
+     
+                    <div className="flex w-[124px] h-[39px] items-center justify-center gap-[10px] p-[10px] relative bg-[#000000cf] rounded-[7px]">
+      <div className="relative w-fit mt-[-1.00px] font-lato font-semibold text-white text-[16px] tracking-[0] leading-[normal] whitespace-nowrap">
+        Book now
+      </div>
+    </div>
+       
+    )
+}
+
+export default Button
---
+++ customer-application/src/components/Line.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Line() {
+    return (
+        <div className="flex justify-center">
+        <div className="w-[1317px] h-[1px] bg-[#000000cf] rounded-[7px]">
+          
+        </div>
+        </div>
+      );
+}
+
+export default Line;
